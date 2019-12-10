ae65cae8dda098866e6466234365114a64c03799
Odoo Aktiv <nupur.soni@aktivsoftware.com>
2019-12-10 11:10:13 +0530

    [ADD]sharevault changes

---
+++ sharevault/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ShareVault',
+    'version': '1.2',
+    'category': 'Tools',
+    'author': 'Wibtec',
+    'website': 'www.wibtec.com',
+    'summary': 'Setup for ShareVault',
+    'description': """
+
+Version(1.1) - ShareVault data model
+Version(1.2) - Added constraint on the email field for partner.
+""",
+    'depends': [
+                'crm',
+                #'auditlog',
+                'helpdesk',
+                'sale_management',
+                'account',
+                #'sic_code'
+                ],
+    'data': [
+            'security/groups_data.xml',
+            'security/ir.model.access.csv',
+            #'data/auditlog_data.xml',
+            #'data/generic_data.xml',
+            'views/sharevault_view.xml',
+
+            'views/partner_view.xml',
+            'views/crm_view.xml',
+
+            'views/templates.xml',
+            ],
+    'installable': True,
+    'auto_install': False,
+}
