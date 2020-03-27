8150fba03aa96139c2abc9df1f694f02722a3da7
rajaktiv <raj.joshi@aktivsoftware.com>
2020-03-27 16:19:42 +0530

    [IMP] removed unused .xml file from views

--- sharevault_import/__manifest__.py
+++ sharevault_import/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ShareVault - changes to contact import',
-    'version': '13.0',
+    'version': '13.0.1.0.0',
     'category': 'Tools',
     'author': 'Wibtec',
     'website': 'www.wibtec.com',
