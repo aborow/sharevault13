745b03efefba431f72d2c78f167d8c08ad43345f
rajaktiv <raj.joshi@aktivsoftware.com>
2020-03-27 17:31:36 +0530

    [ADD] added changes in sharevault module as staging2

--- sharevault/models/partner.py
+++ sharevault/models/partner.py
@@ -196,7 +196,7 @@ class Partner(models.Model):
                                     ], 'Vendor Type')
     opt_out = fields.Boolean('Opt Out')
     source = fields.Text('Source ID')
-    # SV-68
+    #SV-68
     sic_code = fields.Char('SIC Code')
     naics_code = fields.Char('NAICS Code')
     zoom_company_id = fields.Char('Zoom Company ID')
@@ -215,14 +215,40 @@ class Partner(models.Model):
             existing_id = self.search([('email','=', vals['email'])])
             if existing_id:
                 raise ValidationError(_('An email of partner could be defined only one time for one partner.'))
+        """
+        if vals.get('name'):
+            existing_name = self.search([('name', '=', vals['name'])])
+            if existing_name.company_type == 'company':
+                raise ValidationError(_('Name of company must be unique'))
+        if vals.get('domain'):
+            existing_domain = self.search([('domain', '=', vals['domain'])])
+            if existing_domain:
+                raise ValidationError(_('Domain of partner must be unique'))
+        """
         return super(Partner ,self).create(vals)
 
     def write(self,vals):
-        if 'email' in vals:
-            if vals['email']:
-                existing_id = self.search([('email','=', vals['email'])])
-                if existing_id:
-                    raise ValidationError(_('An email of partner could be defined only one time for one partner.'))
+        for s in self:
+            if 'email' in vals:
+                if vals['email']:
+                    existing_id = self.env['res.partner'].search([
+                                                ('email','=', vals['email']),
+                                                ('id','!=', s.id)
+                                                ])
+                    if existing_id:
+                        raise ValidationError(_('An email of partner could be defined only one time for one partner.'))
+            """
+            if 'name' in vals:
+                if vals['name']:
+                    existing_name = self.search([('name', '=', vals['name'])])
+                    if existing_name.company_type == 'company':
+                        raise ValidationError(_('Name of company must be unique'))
+            if 'domain' in vals:
+                if vals['domain']:
+                    existing_domain = self.search([('domain', '=', vals['domain'])])
+                    if existing_domain:
+                        raise ValidationError(_('Domain of partner must be unique'))
+            """
         return super(Partner ,self).write(vals)
 
     @api.onchange('name')
