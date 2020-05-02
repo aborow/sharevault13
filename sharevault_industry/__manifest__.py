ab54fc9f0993d5e6bb07382dde7e773394e5b1aa
Rui Pedrosa Franco <rui.franco@gmail.com>
2020-05-02 15:26:09 +0100

    [UPD] sharevault_industry

---
+++ sharevault_industry/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ShareVault - industry tags',
+    'version': '13.0.1.0.0',
+    'category': 'Tools',
+    'author': 'Wibtec',
+    'website': 'www.wibtec.com',
+    'summary': 'Management of industry-related tags',
+    'description': """
+This module actually changes the code in 'sharevault' in order to deal with
+subindustries in a different way.
+""",
+    'depends': [
+                'sharevault',
+                ],
+    'data': [
+            'security/ir.model.access.csv',
+            'views/partner_view.xml',
+            ],
+    'installable': True,
+    'auto_install': False,
+}
