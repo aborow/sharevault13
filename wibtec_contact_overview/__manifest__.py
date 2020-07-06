8ef2c5b2953c569c15261b486c7412a9b8b911c2
rajaktiv <raj.joshi@aktivsoftware.com>
2020-07-06 19:58:28 +0530

    [ADD] SV-108

---
+++ wibtec_contact_overview/__manifest__.py
@@ -0,0 +1,17 @@
+#! -*- encoding: utf-8 -*-
+{
+    'name': "Wibtec Contact Overview",
+    'version': '13.0.1.0.0',
+    'author': 'Wibtec',
+    'website': 'http://www.wibtec.com',
+    'category': 'Extra Tools',
+    'summary': '',
+    'description': '',
+    'depends': [
+        'base', 'board', 'crm', 'account', 'sale'
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'installable': True,
+}
