dc62e8b33fcef04926fe21fb62701eaf84ec76af
rajaktiv <raj.joshi@aktivsoftware.com>
2020-09-10 21:41:55 +0530

    [ADD] SV-120: Landing page migration POC

---
+++ website_custom_pages/__manifest__.py
@@ -0,0 +1,16 @@
+#! -*- encoding: utf-8 -*-
+{
+    'name': "Website Custom Pages",
+    'version': '13.0.1.0.0',
+    'author': 'Wibtec',
+    'website': 'http://www.wibtec.com',
+    'category': 'Website',
+    'summary': 'Custom pages snippets as per hubspot',
+    'description': """SV-120: Landing page migration POC
+    """,
+    'depends': ['website'],
+    'data': [
+            'views/snippets.xml'
+            ],
+    'installable': True,
+}
