653880b0e255add6dfc62c69da3af9e16cdc07cf
Rui Pedrosa Franco <rui.franco@gmail.com>
2020-10-13 15:05:34 +0100

    [UPD] website_custom_pages

--- website_custom_pages/__manifest__.py
+++ website_custom_pages/__manifest__.py
@@ -1,7 +1,7 @@
 #! -*- encoding: utf-8 -*-
 {
     'name': "Website Custom Pages",
-    'version': '13.0.1.0.20',
+    'version': '13.0.1.0.21',
     'author': 'Wibtec',
     'website': 'http://www.wibtec.com',
     'category': 'Website',
