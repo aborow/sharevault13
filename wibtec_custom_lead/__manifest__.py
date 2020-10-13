db87b7ca5e80416229173a865edf091cbdec1073
Rui Pedrosa Franco <rui.franco@gmail.com>
2020-10-13 15:51:05 +0100

    [UPD] wibtec_custom_lead
    Send SMS on lead creation

--- wibtec_custom_lead/__manifest__.py
+++ wibtec_custom_lead/__manifest__.py
@@ -1,14 +1,20 @@
 #! -*- encoding: utf-8 -*-
 {
     'name': "Wibtec Lead Customization",
-    'version': '13.0.1.0.2',
+    'version': '13.0.1.0.3',
     'author': 'Wibtec',
     'website': 'http://www.wibtec.com',
     'category': 'Leads',
-    'summary': 'SV-141: Lead Customization',
-    'depends': ['base', 'crm', ],
+    'summary': 'SV-141: Lead Customization, SMS sending',
+    'depends': [
+                'base',
+                'crm',
+                'sms',
+                'base_automation'
+                ],
     'data': [
             'data/crm_stage_data.xml',
+            'data/base_automation_data.xml',
             'views/crm_lead_view.xml',
             ],
     'installable': True,
