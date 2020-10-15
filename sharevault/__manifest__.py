ab7d4f0b47e5a9eff4c18d35ffa33678c9ff1f90
Rui Pedrosa Franco <rui.franco@gmail.com>
2020-10-15 11:22:11 +0100

    [UPD] sharevault

--- sharevault/__manifest__.py
+++ sharevault/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ShareVault',
-    'version': '13.0.1.0.29',
+    'version': '13.0.1.0.30',
     'category': 'Tools',
     'author': 'Wibtec',
     'website': 'www.wibtec.com',
