939654679bd7eb380bfa55ee7299627112e471ae
Rui Pedrosa Franco <rui.franco@gmail.com>
2020-11-10 10:05:40 +0000

    [UPD] sharevault - accept conditions

--- sharevault/models/crm.py
+++ sharevault/models/crm.py
@@ -19,6 +19,7 @@ class Lead(models.Model):
     original_source = fields.Char('Original Source')
     recycled = fields.Boolean('Recycled')
 
+    accept_conditions = fields.Boolean('SV Conditions accepted')
 
     """
     lead_id
