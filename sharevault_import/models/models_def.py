7be5f11853bc57e6f4abceff3cde221b8420d5b7
Rui Pedrosa Franco <rui.franco@gmail.com>
2020-11-10 10:26:13 +0000

    [UPD] sharevault_import
    comment on attempt to update sharevault module

--- sharevault_import/models/models_def.py
+++ sharevault_import/models/models_def.py
@@ -188,6 +188,7 @@ class Base(models.AbstractModel):
     _inherit = 'base'
 
     def _load_records(self, data_list, update=False):
+        """
         if DEBUG_MODE:
             _logger.info("_load_records SUPER")
         # We prepare the data according to SV's rules
@@ -213,3 +214,4 @@ class Base(models.AbstractModel):
                         _logger.error("ERROR: %s" % e)
                         pass
         return super(Base, self)._load_records(data_list, update=update)
+        """
