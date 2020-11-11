73864ed202ca0a000c0c31277ac1abb76cde9207
Rui Pedrosa Franco <rui.franco@gmail.com>
2020-11-11 16:35:47 +0000

    [ADD] sharevault_custom

---
+++ sharevault_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'ShareVault Custom',
+    'version': '13.0.1.0.38',
+    'category': 'Tools',
+    'author': 'Wibtec',
+    'website': 'www.wibtec.com',
+    'summary': 'Custom changes in ShareVault',
+    'description': """
+- Hubspt fields
+""",
+    'depends': [
+                'contacts',
+                ],
+    'data': [
+            'views/partner_view.xml',
+            ],
+    'installable': True,
+    'auto_install': False,
+}
