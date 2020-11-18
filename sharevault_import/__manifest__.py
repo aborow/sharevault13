c58fbc350cd25089ac038aba098d361e14f23342
Riya <riya@aktivsoftware.com>
2020-11-19 00:45:13 +0530

    [ADD] Add it back

--- sharevault_import/__manifest__.py
+++ sharevault_import/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ShareVault - changes to contact import',
-    'version': '13.0.1.0.2',
+    'version': '13.0.1.0.1',
     'category': 'Tools',
     'author': 'Wibtec',
     'website': 'www.wibtec.com',
