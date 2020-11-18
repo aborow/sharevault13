773719cf227315abc16d136c9831577924a9197e
Riya <riya@aktivsoftware.com>
2020-11-19 00:04:28 +0530

    [UPDATE] update version

--- sharevault_import/__manifest__.py
+++ sharevault_import/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ShareVault - changes to contact import',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.0.1',
     'category': 'Tools',
     'author': 'Wibtec',
     'website': 'www.wibtec.com',
@@ -12,13 +12,13 @@
     'depends': [
                 'sharevault',
                 'contacts',
-                'base_import'
-                ],
+                'base_import',
+    ],
     'data': [
-            'data/ir_sequence_data.xml',
-            'data/data.xml',
-            'view/partner_view.xml'
-            ],
+        'data/ir_sequence_data.xml',
+        'data/data.xml',
+        'view/partner_view.xml'
+    ],
     'installable': True,
     'auto_install': False,
 }
