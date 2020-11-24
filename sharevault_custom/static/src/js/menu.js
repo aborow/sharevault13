af5b6029f2d1c913a5ba249274e6aaf5e2d6baed
Rui Pedrosa Franco <rui.franco@gmail.com>
2020-11-24 11:56:04 +0000

    Revert "[REM] sharevault_custom"
    This reverts commit 33d77e73214c099bdaee68a14ff46cb8b2d16d1c.

---
+++ sharevault_custom/static/src/js/menu.js
@@ -0,0 +1,39 @@
+odoo.define('sharevault_custom.menu', function (require) {
+'use strict';
+
+var dom = require('web.dom');
+var publicWidget = require('web.public.widget');
+var wUtils = require('website.utils');
+publicWidget.registry.affixMenu = publicWidget.Widget.extend({
+    selector: 'header.o_affix_enabled',
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Called when the window is resized or scrolled -> updates affix status and
+     * automatically closes submenus.
+     *
+     * @private
+     */
+    _onWindowUpdate: function () {
+        if (this.$navbarCollapses.hasClass('show')) {
+            return;
+        }
+
+        var wOffset = $(window).scrollTop();
+        var hOffset = this.$target.scrollTop();
+
+        // THIS IS HOW IT WAS
+        //this.$headerClone.toggleClass('affixed', wOffset > (hOffset + 300));
+
+        // THIS IS HOW IT SHOULD BE
+        this.$headerClone.toggleClass('affixed', wOffset > (hOffset - 50));
+
+        // Reset opened menus
+        this.$dropdowns.add(this.$dropdownMenus).removeClass('show');
+        this.$navbarCollapses.removeClass('show').attr('aria-expanded', false);
+    },
+    });
+});
