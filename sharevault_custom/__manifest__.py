39b0a7bc0e8479e83d0903144f4094a9f46d130f
rajaktiv <raj.joshi@aktivsoftware.com>
2020-11-24 16:40:44 +0530

    [ADD] Keep website header fixed

--- sharevault_custom/__manifest__.py
+++ sharevault_custom/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ShareVault Custom',
-    'version': '13.0.1.0.1',
+    'version': '13.0.1.0.2',
     'category': 'Tools',
     'author': 'Wibtec',
     'website': 'www.wibtec.com',
