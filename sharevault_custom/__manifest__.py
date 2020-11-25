6c6d55ee458e5587e7610d8466f2d1c34fe2f574
rajaktiv <raj.joshi@aktivsoftware.com>
2020-11-25 17:09:17 +0530

    [MOD]Modified changes for fix header in website

--- sharevault_custom/__manifest__.py
+++ sharevault_custom/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ShareVault Custom',
-    'version': '13.0.1.0.2',
+    'version': '13.0.1.0.3',
     'category': 'Tools',
     'author': 'Wibtec',
     'website': 'www.wibtec.com',
