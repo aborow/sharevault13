eff8841db4043f39e58422879fab00908c773d0f
rajaktiv <raj.joshi@aktivsoftware.com>
2020-11-26 19:51:52 +0530

    [ADD]SV-171: Update Sales Order template

---
+++ sharevault_sale_report/__manifest__.py
@@ -0,0 +1,19 @@
+#! -*- encoding: utf-8 -*-
+{
+    'name': "ShareVault Sale Report",
+    'version': '13.0.1.0.19',
+    'author' : 'Wibtec',
+    'website': 'http://www.wibtec.com',
+    'category': 'Extra tools',
+    'summary': 'Sales',
+    'description': 'Sales',
+    'depends': [
+                'sharevault',
+                'sale'
+                ],
+    'data': [
+                'views/report_sale_order.xml',
+                'views/sale_views.xml',
+                ],
+    'installable': True,
+}
