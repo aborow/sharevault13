e68c5b9ade5e6757cf81f8e404a7e4734add7dcf
rajaktiv <raj.joshi@aktivsoftware.com>
2020-12-16 19:55:37 +0530

    [IMP] comment automated action code

--- sharevault_website_notification/__manifest__.py
+++ sharevault_website_notification/__manifest__.py
@@ -1,7 +1,7 @@
 #! -*- encoding: utf-8 -*-
 {
     'name': "Sharevault Website Notification",
-    'version': '13.0.1.0.2',
+    'version': '13.0.1.0.3',
     'author': 'Wibtec',
     'website': 'http://www.wibtec.com',
     'category': 'Leads',
