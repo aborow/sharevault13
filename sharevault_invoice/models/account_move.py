a08acc715e6cbacbd4dea1c0f73c510c3b1eea61
Vishmita <vishmita@aktivsoftware.com>
2020-12-23 14:04:11 +0530

    Revert "[IMP][SV-231] hide expiration field,displayed only the internal reference and product name on the deferred revenue record, removed a (US) after state in the Invoice Report."
    This reverts commit 6c7a9bb279ef984402209663fd7b99ab120810aa.

--- sharevault_invoice/models/account_move.py
+++ sharevault_invoice/models/account_move.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import ValidationError
 _logger = logging.getLogger(__name__)
 
 
@@ -106,62 +106,6 @@ class AccountMove(models.Model):
             if move.invoice_payment_state == 'paid':
                 if move.amount_residual == 0.0:
                     move.amount_paid = move.amount_total
-
-    def _auto_create_asset(self):
-        create_list = []
-        invoice_list = []
-        auto_validate = []
-        for move in self:
-            if not move.is_invoice():
-                continue
-
-            for move_line in move.line_ids.filtered(lambda line: not (move.type in ('out_invoice', 'out_refund') and line.account_id.user_type_id.internal_group == 'asset')):
-                if (
-                    move_line.account_id
-                    and (move_line.account_id.can_create_asset)
-                    and move_line.account_id.create_asset != "no"
-                    and not move.reversed_entry_id
-                    and not (move_line.currency_id or move.currency_id).is_zero(move_line.price_total)
-                    and not move_line.asset_id
-                ):
-                    if not move_line.name:
-                        raise UserError(_('Journal Items of {account} should have a label in order to generate an asset').format(account=move_line.account_id.display_name))
-                    vals = {
-                        'name': move_line.product_id.display_name,
-                        'company_id': move_line.company_id.id,
-                        'currency_id': move_line.company_currency_id.id,
-                        'account_analytic_id': move_line.analytic_account_id.id,
-                        'analytic_tag_ids': [(6, False, move_line.analytic_tag_ids.ids)],
-                        'original_move_line_ids': [(6, False, move_line.ids)],
-                        'state': 'draft',
-                    }
-                    model_id = move_line.account_id.asset_model
-                    if model_id:
-                        vals.update({
-                            'model_id': model_id.id,
-                        })
-                    auto_validate.append(move_line.account_id.create_asset == 'validate')
-                    invoice_list.append(move)
-                    create_list.append(vals)
-
-        assets = self.env['account.asset'].create(create_list)
-        for asset, vals, invoice, validate in zip(assets, create_list, invoice_list, auto_validate):
-            if 'model_id' in vals:
-                asset._onchange_model_id()
-                asset._onchange_method_period()
-                if validate:
-                    asset.validate()
-            if invoice:
-                asset_name = {
-                    'purchase': _('Asset'),
-                    'sale': _('Deferred revenue'),
-                    'expense': _('Deferred expense'),
-                }[asset.asset_type]
-                msg = _('%s created from invoice') % (asset_name)
-                msg += ': <a href=# data-oe-model=account.move data-oe-id=%d>%s</a>' % (invoice.id, invoice.name)
-                asset.message_post(body=msg)
-        return assets
-
 
 class AccountMoveLine(models.Model):
 
