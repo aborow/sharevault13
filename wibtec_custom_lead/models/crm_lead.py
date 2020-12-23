7e4f825bd0ded15f8f1c3bda048a6e91c60e7e36
odoo <odoo>
2020-12-24 01:47:48 +0530

    UPdate

--- wibtec_custom_lead/models/crm_lead.py
+++ wibtec_custom_lead/models/crm_lead.py
@@ -52,7 +52,7 @@ class CrmLead(models.Model):
         return self._stage_find(team_id=team.id, domain=[('fold', '=', False)]).id
 
     @api.depends('score', 'partner_id.hubspot_score')
-    def _compute_odoo_score(self):
+    def _compute_score(self):
         for lead in self:
             if lead.partner_id.hubspot_score or lead.score:
                 lead.odoo_score = lead.score + lead.partner_id.hubspot_score
@@ -85,7 +85,7 @@ class CrmLead(models.Model):
     x_is_updated = fields.Boolean('x_is_updated', default=False, copy=False)
     x_last_modified_on = fields.Datetime("SF last Modified.", copy=False)
     salesforce_response = fields.Text('Response')
-    odoo_score = fields.Float('Odoo Score',compute='_compute_odoo_score', store=True)
+    odoo_score = fields.Float('Odoo Score',compute='_compute_score', store=True)
     is_sf_lead = fields.Boolean('Synced from Salesforce', default=False, copy=False)
     sf_last_modified_date = fields.Datetime('Salesforce Last Modified Date')
     sf_last_sync_date = fields.Datetime('Last Synced Date')
