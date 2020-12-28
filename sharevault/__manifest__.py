77ac82b144b51e229eb673708d8666c1ab1fb292
rajaktiv <raj.joshi@aktivsoftware.com>
2020-12-28 14:35:57 +0530

    [FIX] update the sharevault_import code

--- sharevault/__manifest__.py
+++ sharevault/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ShareVault',
-    'version': '13.0.1.0.43',
+    'version': '13.0.1.0.42',
     'category': 'Tools',
     'author': 'Wibtec',
     'website': 'www.wibtec.com',
