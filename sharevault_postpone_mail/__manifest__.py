d918df8105a15d55242ab90f9a42abd0c4159843
Rui Pedrosa Franco <rui.franco@gmail.com>
2021-01-10 15:35:13 +0000

    [UPD] sharevault_postpone_mail

--- sharevault_postpone_mail/__manifest__.py
+++ sharevault_postpone_mail/__manifest__.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ShareVault - postpone mail sending',
-    'version': '13.0.1.0.5',
+    'version': '13.0.1.0.6',
     'category': 'Tools',
     'author': 'Wibtec',
     'website': 'www.wibtec.com',
     'summary': 'Postpone mail sending (for mailings)',
     'description': """
-On certain weekdays, emails should not be sent
+On certain weekdays, mass emails should not be sent.
+Mailings are automatically scheduled according to what is defined in settings.
 """,
     'depends': [
-                'mail',
+                'mass_mailing',
                 'base_automation'
                 ],
     'data': [
